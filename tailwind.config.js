efef59523664ca529412c9dbbf229151ae077b0a
ateals <ateals@icloud.com>
2023-09-26 21:57:51 +0900

    init: tailwind color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            black: {
+                sm: "#202B38",
+                lg: "#0B151D",
+            },
+            white: {
+                100: "#ffffff",
+                200: "#E5EAF0",
+            },
+            red: "tomato",
+        },
         extend: {},
     },
     plugins: [],
